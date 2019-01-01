b48a27ae3b985df4eb6afdec0640eb582a20febe
taqinlaodedie <yuxiao.zhan@gmail.com>
2019-01-01 21:09:02 +0100

    Ajouter l'affichage d'un dictionnaire

--- anagramme.c
+++ anagramme.c
@@ -8,6 +8,14 @@ struct s_lettre
 	char nom;
 	int nombre;
 };
+struct s_dico {
+  char estMot;
+  char estFeuille;
+  char estAnagramme;
+  struct s_dico *suite[26];
+};
+char chaine[100];
+int indice = 0;
 
 struct s_lettre *creerTableauCompeurLettre(char *chaine)
 {
@@ -54,7 +62,7 @@ void afficheTableauCompteurLettre(char *chaine, struct  s_lettre *tab)
 void anagramme(struct s_lettre *tab, int taille, char *chaineAnagramme, char *debAnagramme)
 {
 	int i;
-	
+
 	for(int i = 1; i <= tab[0].nombre; i++) {
 		if(taille == 0) {
 			printf("%s\n",debAnagramme);
@@ -62,7 +70,6 @@ void anagramme(struct s_lettre *tab, int taille, char *chaineAnagramme, char *de
 		}
 		else if(tab[i].nombre > 0) {
 			chaineAnagramme[0] = tab[i].nom;
-
 			tab[i].nombre --;
 			anagramme(tab, --taille, ++chaineAnagramme, debAnagramme);
 			taille ++;
@@ -72,16 +79,102 @@ void anagramme(struct s_lettre *tab, int taille, char *chaineAnagramme, char *de
 	}
 }
 
+struct s_dico *creerNoeudDico(char estFeuille, char estMot)
+{
+	struct s_dico *dico = NULL;
+	dico = malloc(sizeof(struct s_dico));
+
+	if(dico == NULL) {
+		printf("Erreur d'allocation pour dico\n");
+		return NULL;
+	}
+
+	dico->estMot = estMot;
+	dico->estFeuille = estFeuille;
+	dico->estAnagramme = 0;
+}
+
+void insererMot(char *mot, struct s_dico *dictionnaire)
+{
+	int i = 0, j = 0;
+	struct s_dico *dico = dictionnaire;
+	char *lettre = strdup(mot);
+
+	while(*lettre) {
+		i = *lettre - 'a';
+		if(dico->suite[i] == NULL) {
+			dico->suite[i] = creerNoeudDico(0, 0);
+			dico->estFeuille = 0;
+		}
+		dico = dico->suite[i];
+		lettre ++;
+	}
+
+	dico->estMot = 1; //fin du mot
+	dico->estFeuille = 1;
+
+	while(j < 26) {
+		if(dico->suite[j] != NULL) {
+      		dico->estFeuille = 0;
+      		break;
+    	}
+    	j ++;
+	}
+}
+
+struct s_dico *creerDictionnaire(char *nomFichier) 
+{
+	struct s_dico *dico = creerNoeudDico(0, 0);
+	char tmp[100];
+	FILE *f = fopen(nomFichier, "r");
+
+	while(fscanf(f, "%s", tmp) != EOF) {
+		insererMot(tmp, dico);
+	}
+	fclose(f);
+
+	return dico;
+}
+
+void afficherDictionnaire(struct s_dico *dictionnaire)
+{
+	struct s_dico *dico = dictionnaire;
+
+	if(dico->estMot == 1) {
+		chaine[indice] = '\0';
+		printf("%s\n", chaine);
+	}
+
+	for(int i = 0; i < 26; i++) {
+		if(dico->suite[i] != NULL) {
+			chaine[indice] = 'a' + i;
+			indice ++;
+			afficherDictionnaire(dico->suite[i]);
+			indice --;
+		}
+	}
+}
+
+/*
 int main(void)
 {
 	char *chaine = "neon";
-	char out[4];
+	char out[100];
 	struct s_lettre *tab = creerTableauCompeurLettre(chaine);
 	afficheTableauCompteurLettre(chaine, tab);
-	for(; *chaine; chaine++) {
-		anagramme(tab, 4, out, out);
-	}
+	anagramme(tab, 4, out, out);
+	
 	free(tab);
+
+	return 0;
+}
+*/
+
+int main(void)
+{
+	struct s_dico *dico = creerDictionnaire("dico.txt");
+
+	afficherDictionnaire(dico);
 
 	return 0;
 }
